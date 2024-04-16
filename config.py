2a3f8a9343ffd9bb95391d1c263ae2f3824ebf79
Ꮥʜꫝʟɪɴɪ <155555305+SHALINI-69@users.noreply.github.com>
2024-04-16 21:07:34 +0530

    Update config.py
    Signed-off-by: Ꮥʜꫝʟɪɴɪ <155555305+SHALINI-69@users.noreply.github.com>

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("7145590267"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
